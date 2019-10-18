d40e81689b18febf822f79a01aa0d4ba4f77c146
Poonamkumar Parekh <pkparekh95@gmail.com>
2019-10-18 18:12:27 +0530

    Added view tag and some styling in header comp - module6

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import Header from './src/components/header';
 const App = () => {
     return (
         // <Text>Some text</Text>
-        <Header/>
+        <Header headerText={'Albums!'} />
     );
 };
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,34 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // create the component
-const Header = () => {
-    return <Text>Albums!</Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
 };
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#f0f0f0',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowOffset: { width: 0, height: 2 },
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        elevation: 5,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 25
+    }
+}
 
 // make the component available to other parts of the app
 export default Header;
