480e3a64701d8887e6d737cc09f4b9c68aab1b7f
Poonamkumar Parekh <pkparekh95@gmail.com>
2019-10-20 16:01:28 +0530

    [mod7]Added albumdetail comp with boilerplate

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    //         .then(response => this.setState({ albums: response.data }));
+    // }
+
+    componentDidMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
     
     render() {
         return (
             <View>
-                <Text>AlbumList!!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     };
