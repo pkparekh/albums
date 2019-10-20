837e0deb6863789000c60d1dbfcf667843e2b108
Poonamkumar Parekh <pkparekh95@gmail.com>
2019-10-20 15:25:53 +0530

    [mod7]Restructured albumlist to class comp and added axios call

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>AlbumList!!</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+    
+    render() {
+        return (
+            <View>
+                <Text>AlbumList!!!!</Text>
+            </View>
+        );
+    };
+}
 
 export default AlbumList;
